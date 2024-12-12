2cfd51ba0b783e629964bdc128c50ab65303a607
daichi0525 <dai.0525n@gmail.com>
2024-12-12 09:33:59 +0900

    iconの追加

---
+++ Level0/Level0/Level0/App6-10/TextDisplayApp.swift
@@ -0,0 +1,18 @@
+//
+//  TextDisplayApp.swift
+//  Level0
+//
+//  Created by nakajimadaichi on 2024/12/11.
+//
+
+import SwiftUI
+
+struct TextDisplayApp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextDisplayApp()
+}
